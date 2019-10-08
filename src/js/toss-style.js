05d83d661657798712afb46719c09ddc0e4b4457
Pramod Repaka <200411798@student.georgianc.on.ca>
2019-10-08 14:34:26 -0400

    Code Cleanup

--- src/js/toss-style.js
+++ src/js/toss-style.js
@@ -1,7 +1,7 @@
  //Click on any li in ul with class 'navbar-nav'
  $('ul.navbar-nav li').click(function(e) {
-    var $this = $(this); // declare variable for current li that we click
-    $('ul.navbar-nav').find('li.active').last().removeClass('active') //find all li that class class active and remove
+    var $this = $(this); // Declare variable for current li that we click
+    $('ul.navbar-nav').find('li.active').last().removeClass('active') //Find all li that class class active and remove
     $('ul.navbar-nav').find('li.menuActive').last().removeClass('menuActive')
-    $this.addClass('active menuActive'); //add 'active' class to current li.
+    $this.addClass('active menuActive'); //Add 'active' class to current li.
   });
